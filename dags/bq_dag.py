29f3ddeb8f3c94b979c4ce0fe2ea9f7503bf91f0
tungp3723 <151829885+tungp3723@users.noreply.github.com>
2026-05-11 21:52:08 +0700

    Update bq_dag.py

--- dags/bq_dag.py
+++ dags/bq_dag.py
@@ -1,4 +1,6 @@
+import os
 import pendulum
+
 from airflow import DAG
 from datetime import timedelta
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
@@ -9,21 +11,33 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobO
 PROJECT_ID = "project-8bf7907a-2104-49b0-99f"
 LOCATION = "asia-southeast1"
 
-SQL_BASE_PATH = os.path.join(os.path.dirname(DAG_FOLDER), 'data', 'BQ')
+# Lấy thư mục hiện tại của file DAG
+DAG_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# SQL files nằm trong thư mục: dags/data/BQ/
+SQL_BASE_PATH = os.path.join(DAG_DIR, "data", "BQ")
+
 SQL_FILE_PATH_1 = os.path.join(SQL_BASE_PATH, "bronze.sql")
 SQL_FILE_PATH_2 = os.path.join(SQL_BASE_PATH, "silver.sql")
 SQL_FILE_PATH_3 = os.path.join(SQL_BASE_PATH, "gold.sql")
 
-# Read SQL query from file
+
+# =========================
+# READ SQL FILE
+# =========================
 def read_sql_file(file_path: str) -> str:
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
+
 
 BRONZE_QUERY = read_sql_file(SQL_FILE_PATH_1)
 SILVER_QUERY = read_sql_file(SQL_FILE_PATH_2)
 GOLD_QUERY = read_sql_file(SQL_FILE_PATH_3)
 
-# Define default arguments
+
+# =========================
+# DEFAULT ARGS
+# =========================
 default_args = {
     "owner": "TungPD20",
     "retries": 1,
@@ -33,6 +47,7 @@ default_args = {
     "email_on_retry": False,
 }
 
+
 # =========================
 # DAG
 # =========================
@@ -41,12 +56,12 @@ with DAG(
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
     schedule=None,
     catchup=False,
-    description="DAG to run the bigquery jobs",
+    description="DAG to run BigQuery jobs",
     default_args=default_args,
-    tags=["gcs", "bq", "etl/elt"],
+    tags=["gcs", "bq", "etl", "elt"],
 ) as dag:
 
-    # Task to create bronze table
+    # Task 1: Create bronze tables
     bronze_tables = BigQueryInsertJobOperator(
         task_id="bronze_tables",
         project_id=PROJECT_ID,
@@ -60,7 +75,7 @@ with DAG(
         },
     )
 
-    # Task to create silver table
+    # Task 2: Create silver tables
     silver_tables = BigQueryInsertJobOperator(
         task_id="silver_tables",
         project_id=PROJECT_ID,
@@ -74,7 +89,7 @@ with DAG(
         },
     )
 
-    # Task to create gold table
+    # Task 3: Create gold tables
     gold_tables = BigQueryInsertJobOperator(
         task_id="gold_tables",
         project_id=PROJECT_ID,
@@ -88,5 +103,5 @@ with DAG(
         },
     )
 
-# Task dependencies
-bronze_tables >> silver_tables >> gold_tables
+    # Task dependencies
+    bronze_tables >> silver_tables >> gold_tables
